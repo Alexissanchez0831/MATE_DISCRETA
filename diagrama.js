cf2154f5199b969cdd46866271f0aaa7f5030a64
Alexissanchez0831 <131718958+Alexissanchez0831@users.noreply.github.com>
2023-04-24 21:21:32 -0500

    ok

--- diagrama.js
+++ diagrama.js
@@ -1,43 +1,52 @@
+
 // Definir los datos para los conjuntos
-var sets = [
- {label: "Conjunto A", size: 10},
- {label: "Conjunto B", size: 10},
- {label: "Conjunto C", size: 10}
-];
+var sets: {
+ label: "Conjunto A", size: 10,
+ label: "Conjunto B", size: 10,
+ label: "Conjunto C", size: 10,
+;}
 
 // Definir la configuración para el diagrama de Venn
-var config = 
-{
- sets: sets,
- size: 200,
- padding: 0.1,
- strokeWidth: 2,
- fillOpacity: 0.5,
- strokeOpacity: 1
+var config:{
+    sets: sets,
+    size: 200,
+    padding: 0.1,
+    strokeWidth: 2,
+    fillOpacity: 0.5,
+    strokeOpacity: 1,
 };
 
 // Crear el contenedor SVG para el diagrama de Venn
-var svg = d3.select("#venn-container")
- .append("svg")
+var svg: d3.select("#venn-container")
+ any.append("svg")
  .attr("width", 400)
  .attr("height", 400);
 
 // Crear el diagrama de Venn
-var diagram = venn.VennDiagram()
- .width(config.size)
- .height(config.size)
- .padding(config.padding)
- .strokeWidth(config.strokeWidth)
- .fillOpacity(config.fillOpacity)
- .strokeOpacity(config.strokeOpacity);
+var diagram: venn.VennDiagram 
+{
+    (property) size: 200
+    sets: sets;
+    size: 200;
+    padding: 0.1;
+    strokeWidth: 2;
+    fillOpacity: 0.5;
+    strokeOpacity: 1;
+}
 
 // Añadir los conjuntos al diagrama de Venn
-svg.datum(sets).call(diagram);
+var svg: d3.select 
+{
+    var sets: {
+    label: "Conjunto A";
+    size: 10;
+    .call(var diagram: venn.VennDiagram);
+}
 
 // Añadir las etiquetas a los conjuntos
-svg.selectAll(".venn-label")
- .style("font-size", "16px")
- .style("font-weight", "bold");
+var svg: d3.select("anny")
+var style("font-size", "16px")
+.style("font-weight", "bold");
 
 
    
